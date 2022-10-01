b41b29b222786f023d17ccf62866d84fcbbbc391
YrChek <92719549+YrChek@users.noreply.github.com>
2022-10-01 18:35:48 +0500

    Внесены изменения

--- task.js
+++ task.js
@@ -1,22 +1,18 @@
-const modal = document.getElementById('modal_main')
-const modal2 = document.getElementById('modal_success')
-modal.classList.add('modal_active')
+const modal = document.getElementById('modal_main');
+const modal2 = document.getElementById('modal_success');
+modal.classList.add('modal_active');
+const a = document.querySelector('.btn_danger');
 
-const close = document.querySelectorAll('.modal__close_times')
-const closeMain = close[0];
-const closeSuccess = close[1]
-
-closeMain.onclick = () => {
-    modal.classList.remove('modal_active')
+function close() {
+    this.closest('.modal').classList.remove('modal_active');
 }
-closeSuccess.onclick = () => {
-    // modal.classList.remove('modal_active')
-    modal2.classList.remove('modal_active')
-}
-
-const a = document.querySelector('.btn_danger')
 
 a.onclick = () => {
-    modal2.classList.add('modal_active')
-    modal.classList.remove('modal_active')
+    modal2.classList.add('modal_active');
+    modal.classList.remove('modal_active');
 }
+
+const w = document.querySelectorAll('.modal__close_times')
+w.forEach((r) => {
+    r.onclick = close;
+})
